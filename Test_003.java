a45a5b3cd2db70a5c55e44fc79f9c4dedb4cc29b
Katarinchik <38136000+Katarinchik@users.noreply.github.com>
2022-05-25 20:15:13 +0300

    Update Test_003.java

--- Test_003.java
+++ Test_003.java
@@ -3,7 +3,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
-
+import java.lang.Math;
 public class Bai3TestRunner {
 
 	@Test
