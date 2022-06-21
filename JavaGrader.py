f7642b2aeaf94c9c0d43e6bbafd2780ac0b609ff
Katarinchik <38136000+Katarinchik@users.noreply.github.com>
2022-06-21 19:57:45 +0300

    Update JavaGrader.py

--- JavaGrader.py
+++ JavaGrader.py
@@ -40,12 +40,13 @@ def grade(problem_name, student_response, hide_answer):
     out, err = p.communicate()
     p = subprocess.Popen(["java", "-classpath", "/edx/my-grader:/edx/my-grader/junit-4.11.jar:/edx/my-grader/hamcrest-core-1.3.jar", test_runner], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
-    print(err + "Something wrong")
+    print(out)
     if (err != ""):
         result.update({"correct": False, "error": (err)})
         result = create_response(result, hide_answer)
         return result
     out = re.split("\n", out)
+    print(out)
     keys = ["correct", "function", "result", "expected"]
     print(out)
     for i in range(len(out)):
