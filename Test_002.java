a5d0052f5daa57b3b20509cc9b9cd38b15d3a18e
Katarinchik <38136000+Katarinchik@users.noreply.github.com>
2022-06-22 21:51:41 +0300

    Update Test_002.java

--- Test_002.java
+++ Test_002.java
@@ -10,20 +10,25 @@ public class Test_002 {
 	@Test
 	public void test() {
 		Program p = new Program();
-		if (p.Myfunc(1)==1)
+		for (int i=0;i<10;i++)
 		{
-		System.out.println("True Myfunc(1) 1 1");
-		}
-		else 
-		{
-			System.out.println("True Myfunc(1) "+p.Myfunc(1)+" 1");
-		}
-		assertEquals(1, p.Myfunc(2));
-		assertEquals(2, p.Myfunc(3));
-		assertEquals(3, p.Myfunc(4));
-		assertEquals(5, p.Myfunc(5));
-		assertEquals(8, p.Myfunc(6));
 		
+		System.out.println((p.Myfunc(i)==solution(i)) +" Myfunc("+i+") "+p.Myfunc(1)+" "+ solution(i));
+		
+		}
+		
+		
+	}
+	public int solution(int x) {
+		int n0 = 1;
+		int n1 = 1;
+		int n2;
+		for(int i = 3; i <= x; i++){
+			n2=n0;
+			n0=n1+n0;
+			n1=n2;
+		}
+		return n1;
 	}
 
 	public static void main(String[] args) {
