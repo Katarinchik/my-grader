1607f20fea30d70a0f3e2fee6970e696f5f4e400
Katarinchik <38136000+Katarinchik@users.noreply.github.com>
2022-06-22 20:38:42 +0300

    Update Test_002.java

--- Test_002.java
+++ Test_002.java
@@ -11,7 +11,7 @@ public class Test_002 {
 	public void test() {
 		Program p = new Program();
 		String mes ="";
-		assertEquals(1, p.Myfunc(1))	
+		assertEquals(1, p.Myfunc(1));	
 		assertEquals(1, p.Myfunc(2));
 		assertEquals(2, p.Myfunc(3));
 		assertEquals(3, p.Myfunc(4));
