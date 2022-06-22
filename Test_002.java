a0120c19014a5aa6f582722116a3f40d6be20a17
Katarinchik <38136000+Katarinchik@users.noreply.github.com>
2022-06-22 21:14:37 +0300

    Update Test_002.java

--- Test_002.java
+++ Test_002.java
@@ -11,13 +11,21 @@ public class Test_002 {
 	public void test() {
 		Program p = new Program();
 		String mes ="";
-		assertEquals(mes,1, p.Myfunc(1));	
+		assertEquals(mes,1, p.Myfunc(1));
+		if (mes.isEmpty())
+		{
+		System.out.println("True Myfunc(1) 1 1");
+		}
+		else
+		{
+		System.out.println("False Myfunc(1) "+ p.Myfunc(1)+ " 1");
+		}
 		assertEquals(1, p.Myfunc(2));
 		assertEquals(2, p.Myfunc(3));
 		assertEquals(3, p.Myfunc(4));
 		assertEquals(5, p.Myfunc(5));
 		assertEquals(8, p.Myfunc(6));
-		System.out.println(mes);
+		
 	}
 
 	public static void main(String[] args) {
