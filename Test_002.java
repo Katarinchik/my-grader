852475f57ccd0e3b19a1434028fd41a992d40c45
Katarinchik <38136000+Katarinchik@users.noreply.github.com>
2022-06-22 19:44:02 +0300

    Update Test_002.java

--- Test_002.java
+++ Test_002.java
@@ -9,12 +9,12 @@ public class Test_002 {
 	@Test
 	public void test() {
 		Program p = new Program();
-		assertEquals(1, p.Puzzle(1));
-		assertEquals(1, p.Puzzle(2));
-		assertEquals(2, p.Puzzle(3));
-		assertEquals(3, p.Puzzle(4));
-		assertEquals(5, p.Puzzle(5));
-		assertEquals(8, p.Puzzle(6));
+		assertEquals(1, p.Myfunc(1));
+		assertEquals(1, p.Myfunc(2));
+		assertEquals(2, p.Myfunc(3));
+		assertEquals(3, p.Myfunc(4));
+		assertEquals(5, p.Myfunc(5));
+		assertEquals(8, p.Myfunc(6));
 	}
 
 	public static void main(String[] args) {
