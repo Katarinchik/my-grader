1a198df07f978fa243fcc85d799ec85acd19952d
Katarinchik <38136000+Katarinchik@users.noreply.github.com>
2022-06-22 21:23:31 +0300

    Update Test_002.java

--- Test_002.java
+++ Test_002.java
@@ -16,10 +16,6 @@ public class Test_002 {
 		{
 		System.out.println("True Myfunc(1) 1 1");
 		}
-		else
-		{
-		System.out.println(mes);
-		}
 		assertEquals(1, p.Myfunc(2));
 		assertEquals(2, p.Myfunc(3));
 		assertEquals(3, p.Myfunc(4));
